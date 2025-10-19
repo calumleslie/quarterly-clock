c11f84928443ccaa60d63480629815dcf4c1f1be
Calum Leslie <calumleslie@gmail.com>
2025-10-19 17:13:49 +0100

    Support years that differ from calendar years
    Some companies use fiscal years of various types to track work and why should
    they be excluded from this nightmare?
    
    The change means we can no longer use a bunch of the utilities from the
    date-fns library directly since our definition of year does not match its. The
    change here instead bundles the concept of a year into a helper class.

--- src/model.js
+++ src/model.js
@@ -1,38 +1,101 @@
 import {
   add,
+  addMonths,
+  addWeeks,
+  addYears,
+  differenceInCalendarDays,
+  differenceInCalendarMonths,
   differenceInWeeks,
-  endOfQuarter,
-  endOfYear,
-  getDayOfYear,
-  getDaysInYear,
-  getISOWeeksInYear,
-  getQuarter,
-  startOfISOWeekYear,
-  startOfQuarter,
-  startOfWeek,
   eachDayOfInterval,
-  eachWeekendOfInterval
+  eachWeekendOfInterval,
+  getISOWeeksInYear,
+  isAfter,
+  startOfISOWeekYear,
+  startOfWeek,
+  subMilliseconds,
+  toDate
 } from "date-fns";
-import startOfYear from "date-fns/startOfYear";
 
-export function modelForDate(now) {
-  const elapsedInWholeDays = getDayOfYear(now);
-  const daysInYear = getDaysInYear(now);
+// Input is expected to be a URLSearchParams instance
+export function parseParameters(params) {
+  // We zero-index months internally because JS does but we don't expose that in the
+  // API since it's weird
+  const yearStartMonth = parseInt(params.get('yearStartMonth') || '1') - 1;
+  return {
+    yearStartMonth
+  };
+}
+
+function currentModelYear(yearStartMonth, now) {
+  // This only works when yearStartMonth is positive; in practice this is all we need, though if
+  // we want to support something beyond quarters you might need to know where the year of a given
+  // number starts relative to the calendar year of that number
+  const yearMatchesCalendar = !isAfter(new Date(now.getFullYear(), yearStartMonth), now);
+  const yearNumber = yearMatchesCalendar ? now.getFullYear() : now.getFullYear() - 1;
+  const yearStart = new Date(yearNumber, yearStartMonth);
+
+  return new ModelYear(yearStart);
+}
+
+// Represents a year period starting on a given date
+class ModelYear {
+  constructor(startDate) {
+    this.startDate = startDate;
+  }
+
+  get endDate() {
+    return subMilliseconds(addYears(this.startDate, 1), 1);
+  }
+
+  get daysInYear() {
+    return this.dayOfYear(addYears(this.startDate, 1)) - 1;
+  }
+
+  dayOfYear(date) {
+    return differenceInCalendarDays(date, this.startDate) + 1;
+  }
+
+  quarterOf(date) {
+    return Math.floor(differenceInCalendarMonths(date, this.startDate) / 3) + 1;
+  }
+
+  startOfQuarter(quarter) {
+    // Quarters are 1-indexed
+    return addMonths(this.startDate, 3 * (quarter - 1));
+  }
+
+  yearFraction(date) {
+    // The code this replaced used 1-indexed days throughout so we do the same here; this
+    // tends to make these calculations off by one
+    const day = this.dayOfYear(date);
+
+    // We calculate proportion through the next year if this doesn't fall within this
+    // year. This still uses the length of _this_ year since the intent is to calculate
+    // an angle around a circle representing this year.
+    return (((day - 1) % this.daysInYear) + 1) / this.daysInYear;
+  }
+}
+
+export function modelForDate(now, args) {
+  now = toDate(now);
+  args = args || {};
+
+  const year = currentModelYear(args.yearStartMonth || 0, now);
+
+  const elapsedInWholeDays = year.dayOfYear(now);
+  const daysInYear = year.daysInYear;
 
   const thisDayButNextWeek = add(now, { weeks: 1 });
   const startOfNextWeek = startOfWeek(thisDayButNextWeek);
-  const startOfNextWeekInWholeDays = getDayOfYear(startOfNextWeek);
 
-  const currentQuarter = getQuarter(now);
-  const startOfCurrentQuarter = startOfQuarter(now);
-  const startOfCurrentQuarterInWholeDays = getDayOfYear(startOfCurrentQuarter);
-  const endOfCurrentQuarter = endOfQuarter(now);
-  const endOfCurrentQuarterInWholeDays = getDayOfYear(endOfCurrentQuarter);
+  const currentQuarter = year.quarterOf(now);
+  const startOfCurrentQuarter = year.startOfQuarter(currentQuarter);
+  const endOfCurrentQuarter = subMilliseconds(year.startOfQuarter(currentQuarter + 1), 1);
   const wholeWeeksLeftInCurrentQuarter = Math.abs(
     differenceInWeeks(endOfCurrentQuarter, startOfNextWeek)
   );
-  const startOfCurrentYear = startOfYear(now);
-  const endOfCurrentYear = endOfYear(now);
+  const startOfCurrentYear = year.startDate;
+  const endOfCurrentYear = year.endDate;
   const isoStart = startOfISOWeekYear(now);
   const isoWeeks = getISOWeeksInYear(now);
   const range = [...Array(isoWeeks).keys()];
@@ -63,26 +126,25 @@ export function modelForDate(now) {
       index: currentQuarter - 1,
       label: `Q${currentQuarter}`,
       start: {
-        yearFraction: startOfCurrentQuarterInWholeDays / daysInYear,
+        yearFraction: year.yearFraction(startOfCurrentQuarter),
       },
       end: {
-        yearFraction: endOfCurrentQuarterInWholeDays / daysInYear,
+        yearFraction: year.yearFraction(endOfCurrentQuarter),
       },
       wholeWeeksLeft: {
         start: {
-          yearFraction: startOfNextWeekInWholeDays / daysInYear,
+          yearFraction: year.yearFraction(startOfNextWeek),
         },
         end: {
           yearFraction:
-            (startOfNextWeekInWholeDays + wholeWeeksLeftInCurrentQuarter * 7) /
-            daysInYear,
+            year.yearFraction(addWeeks(startOfNextWeek, wholeWeeksLeftInCurrentQuarter)),
         },
         durationInWeeks: wholeWeeksLeftInCurrentQuarter,
       },
       availableDays: nonWeekendDays.map((date) => ({
-        day: getDayOfYear(date),
-        start: { yearFraction: getDayOfYear(date) / daysInYear },
-        end: { yearFraction: getDayOfYear(add(date, { days: 1 })) / daysInYear },
+        day: year.dayOfYear(date),
+        start: { yearFraction: year.yearFraction(date) },
+        end: { yearFraction: year.yearFraction(add(date, { days: 1 })) },
       })),
     },
     startOfYear: startOfCurrentYear,
