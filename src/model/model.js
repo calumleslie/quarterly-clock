7ecd4b0800fe40e8f79d579e38163c3644ce7a76
Mike Moran <mike@houseofmoran.com>
2025-11-02 23:19:43 +0000

    extract `YearInterval` into separate file

--- src/model/model.js
+++ src/model/model.js
@@ -1,20 +1,16 @@
 import {
   add,
-  addMonths,
   addWeeks,
-  addYears,
-  differenceInCalendarDays,
-  differenceInCalendarMonths,
   differenceInWeeks,
   eachDayOfInterval,
   eachWeekendOfInterval,
   getISOWeeksInYear,
-  isAfter,
   startOfISOWeekYear,
   startOfWeek,
   subMilliseconds,
   toDate
 } from "date-fns";
+import { currentYearInterval } from "./yearInterval.js";
 
 // Input is expected to be a URLSearchParams instance
 export function parseParameters(params) {
@@ -26,61 +22,11 @@ export function parseParameters(params) {
   };
 }
 
-function currentModelYear(yearStartMonth, now) {
-  // This only works when yearStartMonth is positive; in practice this is all we need, though if
-  // we want to support something beyond quarters you might need to know where the year of a given
-  // number starts relative to the calendar year of that number
-  const yearMatchesCalendar = !isAfter(new Date(now.getFullYear(), yearStartMonth), now);
-  const yearNumber = yearMatchesCalendar ? now.getFullYear() : now.getFullYear() - 1;
-  const yearStart = new Date(yearNumber, yearStartMonth);
-
-  return new YearInterval(yearStart);
-}
-
-// Represents a year interval starting on a given date
-class YearInterval {
-  constructor(startDate) {
-    this.startDate = startDate;
-  }
-
-  get endDate() {
-    return subMilliseconds(addYears(this.startDate, 1), 1);
-  }
-
-  get daysInYear() {
-    return this.dayOfYear(addYears(this.startDate, 1)) - 1;
-  }
-
-  dayOfYear(date) {
-    return differenceInCalendarDays(date, this.startDate) + 1;
-  }
-
-  quarterOf(date) {
-    return Math.floor(differenceInCalendarMonths(date, this.startDate) / 3) + 1;
-  }
-
-  startOfQuarter(quarter) {
-    // Quarters are 1-indexed
-    return addMonths(this.startDate, 3 * (quarter - 1));
-  }
-
-  yearFraction(date) {
-    // The code this replaced used 1-indexed days throughout so we do the same here; this
-    // tends to make these calculations off by one
-    const day = this.dayOfYear(date);
-
-    // We calculate proportion through the next year if this doesn't fall within this
-    // year. This still uses the length of _this_ year since the intent is to calculate
-    // an angle around a circle representing this year.
-    return (((day - 1) % this.daysInYear) + 1) / this.daysInYear;
-  }
-}
-
 export function modelForDate(now, args) {
   now = toDate(now);
   args = args || {};
 
-  const year = currentModelYear(args.yearStartMonth || 0, now);
+  const year = currentYearInterval(args.yearStartMonth || 0, now);
 
   const elapsedInWholeDays = year.dayOfYear(now);
   const daysInYear = year.daysInYear;
